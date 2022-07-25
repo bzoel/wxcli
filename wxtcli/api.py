83f1ad3fce0caafd4b5818c813f83dc2291f698b
Billy Zoellers <billy.zoellers@mac.com>
2022-07-25 13:17:03 -0400

    Webex calling - work in progress, add list-caller-ids

--- wxtcli/api.py
+++ wxtcli/api.py
@@ -6,6 +6,7 @@ Global API object
 """
 import requests
 from webexteamssdk import WebexTeamsAPI
+from wxtcli.console import console
 
 api = WebexTeamsAPI()
 
@@ -28,7 +29,15 @@ def api_req(resource, method="get", **kwargs):
 
     resp.raise_for_status()
 
+    # Handle pagination
+    if "next" in resp.links.keys():
+        next_resource = resp.links["next"]["url"].replace("https://webexapis.com/v1/", "")
+        console.log(next_resource)
+        return resp.json()["items"] + api_req(next_resource)
+
     if resp.text:
+        if "items" in resp.json().keys():
+            return resp.json()["items"]
         return resp.json()
     
     return {}
--- wxtcli/calling.py
+++ wxtcli/calling.py
@@ -6,16 +6,50 @@ People module
 """
 import typer
 from wxtcli.console import console
-from wxtcli.api import api
+from wxtcli.api import api, api_req
 from wxtcli.helpers.formatting import table_with_columns, humanize_wxt_datetime
 from rich.progress import track
 
 app = typer.Typer()
 
 @app.command()
-def template(
+def list_caller_ids(
+    location_name: str
 ):
     """
-    A template function
+    List caller IDs for all people in a location
     """
-    console.print("Hi")
+    # First locationId of interest
+    locations = api_req("locations")
+    locationId = next(location["id"] for location in locations if location["name"] == location_name)
+
+    # Find people belonging to that locationId
+    people = api_req("people", params={
+        "callingData": True,
+        "locationId": locationId,
+    })
+
+    # Find callerId for each person
+    table = table_with_columns(
+        ["Person", "Extension", "Caller ID Type", "Caller ID Number"], title="Caller ID Info"
+    )
+    for person in track(people):
+        caller_id = api_req(f"people/{person['id']}/features/callerId")
+
+        caller_id_number = "Unknown"
+        if caller_id["selected"] == "DIRECT_LINE":
+            caller_id_number = caller_id["directNumber"]
+        elif caller_id["selected"] == "LOCATION_NUMBER":
+            caller_id_number = caller_id["locationNumber"]
+        elif caller_id["selected"] == "CUSTOM":
+            caller_id_number = caller_id["customNumber"]
+
+        table.add_row(
+            person["displayName"],
+            person["extension"],
+            caller_id["selected"],
+            caller_id_number
+        )
+    console.print(table)
+
+
