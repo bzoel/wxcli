eb9b07c7efda40ef4086aeed2ec720ffdac18dad
Billy Zoellers <billy.zoellers@mac.com>
2022-07-25 12:03:14 -0400

    add generic API request

--- wxtcli/api.py
+++ wxtcli/api.py
@@ -4,6 +4,31 @@ Billy Zoellers
 
 Global API object
 """
+import requests
 from webexteamssdk import WebexTeamsAPI
 
 api = WebexTeamsAPI()
+
+def api_req(resource, method="get", **kwargs):
+    """
+    Make an API request to Webex that is not contained
+    within the Python SDK
+    """
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api.access_token}"
+    }
+
+    resp = requests.request(
+        url=f"{api.base_url}/{resource}",
+        method=method,
+        headers=headers,
+        **kwargs
+    )
+
+    resp.raise_for_status()
+
+    if resp.text:
+        return resp.json()
+    
+    return {}
