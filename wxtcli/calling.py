b4ea9c00388234c731672b05b25966e5c42f52a0
dwrice0 <52576449+dwrice0@users.noreply.github.com>
2023-04-27 13:52:47 -0400

    Added all phone apply settings and update all caller ID with orgId (#5)

--- wxtcli/calling.py
+++ wxtcli/calling.py
@@ -109,27 +109,49 @@ def update_user_cid(
 
 @app.command()
 def list_caller_ids(
-    location_name: str = typer.Argument(..., help="Webex Calling Location Name")
+    location_name: str = typer.Argument(..., help="Webex Calling Location Name"),
+    org_id: str = typer.Option(None, help="Organization ID")
 ):
     """
     List caller IDs for all people in a location
     """
+    orgId=None
+    if (org_id is not None):
+        orgId = api_req(f"organizations/{org_id}")["id"]
+
     # First locationId of interest
-    locations = api_req("locations")
+    if (org_id is not None):
+        locations = api_req("locations", params ={
+        "orgId": org_id,
+    })
+    else:  
+        locations = api_req("locations")
     locationId = next(location["id"] for location in locations if location["name"] == location_name)
 
     # Find people belonging to that locationId
-    people = api_req("people", params={
-        "callingData": True,
-        "locationId": locationId,
-    })
+    if (org_id is not None):
+        people = api_req("people", params={
+            "callingData": True,
+            "locationId": locationId,
+            "orgId": orgId,
+        })
+    else:
+        people = api_req("people", params={
+            "callingData": True,
+            "locationId": locationId,
+        })
 
     # Find callerId for each person
     table = table_with_columns(
         ["Person", "Extension", "Caller ID Type", "Caller ID Number"], title="Caller ID Info"
     )
     for person in track(people):
-        caller_id = api_req(f"people/{person['id']}/features/callerId")
+        if (org_id is not None):
+            caller_id = api_req(f"people/{person['id']}/features/callerId", params={
+            "orgId": orgId,
+        })
+        else:
+            caller_id = api_req(f"people/{person['id']}/features/callerId")
 
         caller_id_number = "Unknown"
         if caller_id["selected"] == "DIRECT_LINE":
@@ -151,11 +173,17 @@ def list_caller_ids(
 @app.command()
 def update_location_allcids(
     location_name: str = typer.Argument(..., help="Webex Calling Location Name"),
-    caller_id: str = typer.Argument("LOCATION_NUMBER", help="Caller ID | LOCATION_NUMBER | DIRECT_LINE")
+    caller_id: str = typer.Argument("LOCATION_NUMBER", help="Caller ID | LOCATION_NUMBER | DIRECT_LINE"),
+    org_id: str = typer.Option(None, help="Organization ID")
 ):
     """
     Update caller IDs for all people in a location
     """
+
+    orgId=None
+    if (org_id is not None):
+        orgId = api_req(f"organizations/{org_id}")["id"]
+    
     # Determine Caller ID  type
     caller_id_types = ["LOCATION_NUMBER", "DIRECT_LINE"]
     custom_number = None
@@ -164,28 +192,125 @@ def update_location_allcids(
         caller_id = "CUSTOM"
 
     # First locationId of interest
-    locations = api_req("locations")
+    if (org_id is not None):
+        locations = api_req("locations", params ={
+        "orgId": org_id,
+    })
+    else:  
+        locations = api_req("locations")
     locationId = next(location["id"] for location in locations if location["name"] == location_name)
 
     # Find people belonging to that locationId
-    people = api_req("people", params={
+    if (org_id is not None):
+        people = api_req("people", params={
         "callingData": True,
         "locationId": locationId,
+        "orgId": orgId,
     })
+    else:
+        people = api_req("people", params={
+            "callingData": True,
+            "locationId": locationId,
+        })
 
     # Set callerId for each person
 
     for person in track(people):
         if (custom_number == None):
-            caller_id_resp = api_req(f"people/{person['id']}/features/callerId", method = "put", json = {
-                "selected" : caller_id
-            })
+            if (org_id is not None):
+                caller_id_resp = api_req(f"people/{person['id']}/features/callerId", method = "put", json = {
+                    "selected" : caller_id,
+                }, params= {
+                "orgId": orgId,
+                })
+            else:
+                caller_id_resp = api_req(f"people/{person['id']}/features/callerId", method = "put", json = {
+                    "selected" : caller_id,
+                })
         else:
-            caller_id_resp = api_req(f"people/{person['id']}/features/callerId", method = "put", json = {
-                "selected" : caller_id,
-                "customNumber" : custom_number
-            })
+            if (org_id is not None):
+                caller_id_resp = api_req(f"people/{person['id']}/features/callerId", method = "put", json = {
+                    "selected" : caller_id,
+                    "customNumber" : custom_number,
+                }, params= {
+                "orgId": orgId,
+                })
+            else:
+                caller_id_resp = api_req(f"people/{person['id']}/features/callerId", method = "put", json = {
+                    "selected" : caller_id,
+                    "customNumber" : custom_number
+                })
 
+
+@app.command()
+def all_phone_apply_config(
+    location_name: str = typer.Argument(..., help="Webex Calling Location Name"),
+    org_id: str = typer.Option(None, help="Organization ID")
+):
+    """
+    List caller IDs for all people in a location
+    """
+    orgId=None
+    if (org_id is not None):
+        orgId = api_req(f"organizations/{org_id}")["id"]
+
+    # First locationId of interest
+    if (org_id is not None):
+        locations = api_req("locations", params ={
+        "orgId": org_id,
+    })
+    else:  
+        locations = api_req("locations")
+    locationId = next(location["id"] for location in locations if location["name"] == location_name)
+
+    # Find people belonging to that locationId
+    if (org_id is not None):
+        people = api_req("people", params={
+            "callingData": True,
+            "locationId": locationId,
+            "orgId": orgId,
+        })
+    else:
+        people = api_req("people", params={
+            "callingData": True,
+            "locationId": locationId,
+        })
+
+    # Find callerId for each person
+    table = table_with_columns(
+        ["Person", "Extension", "Phone Model", "MAC Address", "Type"], title="User Phone Info"
+    )
+    for person in track(people):
+        if (org_id is not None):
+            devices = api_req(f"telephony/config/people/{person['id']}/devices", params={
+            "orgId": orgId,
+        })
+        else:
+            devices = api_req(f"telephony/config/people/{person['id']}/devices")
         
+        #console.print(devices["devices"])
+        for device in devices["devices"]:
+            device_type=device["model"]
+            device_mac=device["mac"]
+            table.add_row(
+                person["displayName"],
+                person["extension"],
+                device["model"],
+                device["mac"],
+                device["type"]
+            )
+            if (device["type"] == "SHARED_CALL_APPEARANCE"):
+                console.print(device['id'])
+            #if (device["type"] == "PRIMARY"):
+            #    if (org_id is not None):
+            #        result = api_req(f"telephony/config/devices/{device['id']}/actions/applyChanges/invoke", method="post", params={
+            #            "orgId": orgId,
+            #        })
+            #    else:
+            #        result = api_req(f"telephony/config/devices/{device['id']}/actions/applyChanges/invoke", method="post")
+            
+            #result = api_req(f"telephony/config/devices/{device['id']}/settings")
+            #console.print(result)
+    #console.print(table)            
 
     
